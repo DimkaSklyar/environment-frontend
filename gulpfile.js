12fa67873f767c210b2c4b28c08ffa9ef622b11a
Dmitry <30568388+DimkaSklyar@users.noreply.github.com>
2022-01-09 22:01:29 +0300

    update template

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,19 @@
-var gulp = require('gulp'),
-	gutil = require('gulp-util'),
-	sass = require('gulp-sass'),
-	browsersync = require('browser-sync').create(),
-	concat = require('gulp-concat'),
-	uglify = require('gulp-uglify'),
-	cleancss = require('gulp-clean-css'),
-	rename = require('gulp-rename'),
-	notify = require('gulp-notify'),
-	cssbeautify = require('gulp-cssbeautify')
-del = require('del'),
-	imagemin = require('gulp-imagemin'),
-	pngquant = require('imagemin-pngquant'),
-	cache = require('gulp-cache'),
-	autoprefixer = require('gulp-autoprefixer');
+const gulp = require('gulp');
+const	gutil = require('gulp-util');
+const	sass = require('gulp-sass')(require('sass'));
+const	browsersync = require('browser-sync').create();
+const	concat = require('gulp-concat');
+const	uglify = require('gulp-uglify');
+const	cleancss = require('gulp-clean-css');
+const	rename = require('gulp-rename');
+const	notify = require('gulp-notify');
+const	cssbeautify = require('gulp-cssbeautify');
+const	del = require('del');
+const	imagemin = require('gulp-imagemin');
+const	pngquant = require('imagemin-pngquant');
+const	cache = require('gulp-cache');
+const	autoprefixer = require('gulp-autoprefixer');
+const minify = require('gulp-minify');
 
 gulp.task('browser-sync', function (done) {
 	browsersync.init({
@@ -32,7 +33,7 @@ gulp.task('clean', function () {
 });
 
 gulp.task('img', function () {
-	return gulp.src('src/img/**/*')
+	return gulp.src('src/assets/**/*')
 		.pipe(cache(imagemin({ // С кешированием
 			// .pipe(imagemin({ // Сжимаем изображения без кеширования
 			interlaced: true,
@@ -40,14 +41,14 @@ gulp.task('img', function () {
 			svgoPlugins: [{ removeViewBox: false }],
 			use: [pngquant()]
 		}))/**/)
-		.pipe(gulp.dest('dist/img'));
+		.pipe(gulp.dest('dist/assets'));
 });
 
-let autoprefixBrowsers = ['> 1%', 'last 2 versions', 'firefox >= 4', 'safari 7', 'safari 8', 'IE 10', 'IE 11'];
+const autoprefixBrowsers = ['> 1%', 'last 2 versions', 'firefox >= 4', 'safari 7', 'safari 8', 'IE 10', 'IE 11'];
 
-gulp.task('sass', function () {
-	return gulp.src('src/sass/**/*.sass')
-		.pipe(sass({ outputStyle: 'expand' }).on('error', notify.onError()))
+gulp.task('scss', function () {
+	return gulp.src('src/scss/**/*.scss')
+		.pipe(sass({ outputStyle: 'compressed', includePaths: ['node_modules'] }).on('error', notify.onError()))
 		.pipe(cssbeautify({
 			indent: '  ',
 			openbrace: 'end-of-line',
@@ -67,17 +68,25 @@ gulp.task('sass', function () {
 
 gulp.task('js', function () {
 	return gulp.src([
-		'src/libs/jquery/dist/jquery.min.js',
+		'./node_modules/jquery/dist/jquery.min.js',
 		'src/js/common.js', // Always at the end
 	])
 		.pipe(concat('scripts.min.js'))
+		.pipe(minify({
+			ext:{
+					src:'-debug.js',
+					min:'.js'
+			},
+			exclude: ['tasks'],
+			ignoreFiles: ['.combo.js', '-min.js']
+		}))
 		.pipe(gulp.dest('src/js'))
 		.pipe(browsersync.reload({ stream: true }))
 
 });
 
-gulp.task('watch', gulp.series('sass', 'js', 'browser-sync', function (done) {
-	gulp.watch('src/sass/**/*.sass', gulp.parallel('sass'));
+gulp.task('watch', gulp.series('scss', 'js', 'browser-sync', function (done) {
+	gulp.watch('src/scss/**/*.scss', gulp.parallel('scss'));
 	gulp.watch(['libs/**/*.js', 'src/js/common.js'], gulp.parallel('js'));
 	gulp.watch('src/*.html');
 	done();
@@ -88,27 +97,28 @@ gulp.task('clear', function (callback) {
 	return cache.clearAll();
 })
 
-gulp.task('build', gulp.series('clean', 'img', 'sass', 'js', function (done) {
-	var buildCss = gulp.src([
+gulp.task('build', gulp.series('clean', 'img', 'scss', 'js', function (done) {
+	gulp.src([
 		'src/css/*.css',
 		'src/css/*.min.css'
 	])
 		.pipe(gulp.dest('dist/css'));
 
-	var buildFonts = gulp.src('src/fonts/**/*')
+	gulp.src('src/fonts/**/*')
 		.pipe(gulp.dest('dist/fonts'));
 
-	var buildJs = gulp.src('src/js/**/*')
+	gulp.src('src/js/**/*')
 		.pipe(gulp.dest('dist/js'));
 
-	var buildHtml = gulp.src('src/*.html')
+	gulp.src('src/*.html')
 		.pipe(gulp.dest('dist'));
 
-	var buildPhp = gulp.src('src/*.php')
+	gulp.src('src/*.php')
 		.pipe(gulp.dest('dist'));
 
-	var buildVideo = gulp.src('src/video/*')
+	gulp.src('src/video/*')
 		.pipe(gulp.dest('dist/video'));
+
 	done();
 }));
 
